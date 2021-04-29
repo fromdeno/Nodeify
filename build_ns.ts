20b393f81f07c84b59204c62251a0d7f011ea24b
Thomas Rory Gummerson <thomas@gummerson.no>
2021-04-29 12:23:55 +0200

    No need for build script

--- build_ns.ts
+++
@@ -1,8 +0,0 @@
-const exec = (cwd: string, cmd: string[]) =>
-  Deno.run({
-    cwd,
-    cmd: [...Deno.build.os === "windows" ? ["cmd", "/c"] : [], ...cmd],
-  });
-
-await exec("deno.ns", ["npm", "i"]).status();
-await exec("deno.ns", ["npm", "run", "full"]).status();
