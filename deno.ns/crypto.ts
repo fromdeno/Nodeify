838c54812d8bc38034a2dd3ea4f41d4ccbbec5d3
Thomas Rory Gummerson <thomas@gummerson.no>
2021-04-29 11:04:08 +0200

    Add crypto types

--- deno.ns/crypto.ts
+++ deno.ns/crypto.ts
@@ -1,5 +1,7 @@
+///<reference path="lib.deno.d.ts" />
+
 import { randomFillSync } from 'crypto';
-export const crypto = {
+export const crypto: typeof globalThis.crypto = {
 	getRandomValues(p) {
 		randomFillSync(p);
 		return p;
