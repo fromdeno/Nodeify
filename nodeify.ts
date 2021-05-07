7f15d5e3b47a4f24561dffb23577b40de7cf1a85
Thomas Rory Gummerson <thomas@gummerson.no>
2021-05-07 11:43:36 +0200

    Major refactor#

--- nodeify.ts
+++ nodeify.ts
@@ -1,74 +1,160 @@
 import { writeAll } from "https://deno.land/std@0.95.0/io/util.ts";
+import { toFileUrl } from "https://deno.land/std@0.95.0/path/mod.ts";
+
+const filterDots = (path: string) => {
+  const segments = path.split("/");
+  const result: string[] = [];
+  for (const segment of segments) {
+    if (segment === "..") {
+      result.pop();
+      continue;
+    }
+    if (segment === ".") {
+      continue;
+    }
+    result.push(segment);
+  }
+  return result.join("/");
+};
+
+export const mapPath = (path: string) => {
+  const url = new URL(path, "file://.");
+  const proto = url.protocol.slice(0, -1);
+  const ext = url.pathname.endsWith(".js") ? "" : ".js";
+  return filterDots(`${proto}/${url.hostname}${url.pathname}${ext}`);
+};
+
+const replaceImportsInModule = (
+  path: string,
+  data: string,
+  outDir: string,
+  map: Record<string, string>,
+  fetched: string[],
+) => {
+  const thing = path.endsWith("brotli@v0.1.4/wasm.js");
+  const replaced = data.replace(
+    /from\s*["']([^"']+)["']/g,
+    (_, x: string) => {
+      if (thing) {
+        console.log(_, x);
+        console.log(x);
+      }
+      if (/^https?:/.test(x) && x.endsWith(".js")) {
+        fetchJSModule(x, outDir, map, fetched).catch((err) =>
+          console.error(err.message)
+        );
+      }
+      return 'from "' +
+        (/^https?:/.test(x)
+          ? ((y) => y.startsWith(".") ? y : "../" + y)(
+            "../".repeat(path.split("/").length - 2) + mapPath(x),
+          )
+          : ((y) => y.endsWith(".js") ? y : y + ".js")(
+            x.startsWith(".") ? x : "./" + x,
+          )) +
+        '"';
+    },
+  );
+  if (thing) {
+    console.log(data.slice(0, 100), replaced.slice(0, 100));
+  }
+  return replaced;
+};
+
+export const fetchJSModule = async (
+  url: string,
+  outDir: string,
+  map: Record<string, string>,
+  fetched: string[],
+) => {
+  if (fetched.includes(url)) {
+    return;
+  }
+  fetched.push(url);
+  const res = await fetch(url);
+  const data = await res.text();
+  const path = `${outDir}/${mapPath(url)}`;
+  await Deno.mkdir(
+    path.split("/").slice(0, -1).join("/"),
+    { recursive: true },
+  );
+  const file = await Deno.open(
+    path,
+    { create: true, write: true, truncate: true },
+  );
+  await writeAll(
+    file,
+    new TextEncoder().encode(
+      replaceImportsInModule(path, data, outDir, map, fetched),
+    ),
+  );
+  file.close();
+};
 
 export const build = async (entrypoint: string, outDir: string) => {
   await Deno.remove(outDir).catch(() => {});
   await Deno.mkdir(outDir, { recursive: true });
+  const map: Record<string, string> = {};
+  const fetched: string[] = [];
   const result = await Deno.emit(entrypoint, {
-    compilerOptions: { allowJs: true },
+    compilerOptions: { allowJs: true, checkJs: true },
   });
-
   if (result.diagnostics.length > 0) {
-    console.error(result.diagnostics);
+    for (const diag of result.diagnostics) {
+      if (diag.fileName?.endsWith(".js") && !diag.fileName.endsWith(".ts.js")) {
+        await fetchJSModule(diag.fileName, outDir, map, fetched);
+      }
+    }
+    // console.error(result.diagnostics);
     // Deno.exit(1);
   }
 
-  const map: Record<string, string> = {};
-  const base = new URL(import.meta.url).pathname
-    .split("/")
-    .slice(0, -1)
-    .join("/");
   for (
     const [name, file] of Object.entries(result.files)
       .filter(([name]) => !name.endsWith(".map"))
+      .map(([name, file]) =>
+        name.startsWith("file:")
+          ? ["." + name.slice(toFileUrl(Deno.cwd()).href.length), file]
+          : [name, file]
+      )
   ) {
-    const url = new URL(name);
-    const mapped = url.protocol === "file:"
-      ? `${outDir}/file/${url.pathname.slice(base.length + 1)}`
-      : `${outDir}/${url.protocol.slice(0, -1)}/${url.hostname}/${
-        url.pathname.slice(1)
-      }`;
+    const mapped = mapPath(name);
     map[name] = mapped;
   }
-  for (const [name, file] of Object.entries(result.files)) {
+  for (
+    const [name, file] of Object.entries(result.files).map(([name, file]) =>
+      name.startsWith("file:")
+        ? ["." + name.slice(toFileUrl(Deno.cwd()).href.length), file]
+        : [name, file]
+    )
+  ) {
     const mapped = map[name];
     if (!mapped) {
       continue;
     }
-    await Deno.mkdir(mapped.split("/").slice(0, -1).join("/"), {
+    await Deno.mkdir(`${outDir}/${mapped}`.split("/").slice(0, -1).join("/"), {
       recursive: true,
     });
-    let replaced = file;
-    for (const [importName, mapped] of Object.entries(map)) {
-      replaced = replaced.replaceAll('.ts"', '.ts.js"');
-      replaced = replaced.replaceAll(
-        '"https://',
-        '"' +
-          "../".repeat(
-            Math.max(
-              1,
-              name.split("/").length -
-                (name.startsWith("https://") ? 0 : base.split("/").length) - 2,
-            ),
-          ) +
-          "https/",
-      );
-      replaced = replaced.replaceAll(importName, mapped);
-    }
-    await Deno.writeTextFile(mapped, replaced);
+    const replaced = replaceImportsInModule(name, file, outDir, map, fetched);
+    await Deno.writeTextFile(`${outDir}/${mapped}`, replaced);
   }
-  const mappedEntrypoint = `./file/${entrypoint}.js`;
+  const mappedEntrypoint = mapPath(entrypoint);
   await Deno.writeTextFile(
     `${outDir}/${mappedEntrypoint}`,
-    `import "deno.ns/global";
-import.meta.main = true;
-${await Deno.readTextFile(`${outDir}/${mappedEntrypoint}`)}`,
+    `import.meta.main = true;\n${await Deno.readTextFile(
+      `${outDir}/${mappedEntrypoint}`,
+    )}`,
+  );
+  await Deno.writeTextFile(
+    `${outDir}/index.js`,
+    `import "deno.ns/global";\nexport * from "./${mappedEntrypoint}";\n`,
   );
   await Deno.writeTextFile(
     `${outDir}/package.json`,
     JSON.stringify(
       {
         type: "module",
-        main: mappedEntrypoint,
+        main: "./index.js",
         "dependencies": {
           "deno.ns": "^0.1.1",
         },
@@ -77,32 +163,6 @@ ${await Deno.readTextFile(`${outDir}/${mappedEntrypoint}`)}`,
       "  ",
     ),
   );
-};
-
-export const copy = async (url: string, path: string) => {
-  await Deno.mkdir(path.split("/").slice(0, -1).join("/"), { recursive: true });
-  await Deno.writeTextFile(
-    path,
-    (await (await fetch(url)).text())
-      .replaceAll('.ts"', '.ts.js"')
-      .replaceAll('"https://', '"../../../../https/'),
-  );
-};
-
-const write = async <T>(bundle: T, path: string) => {
-  if (bundle instanceof Uint8Array) {
-    const file = await Deno.open(path, {
-      write: true,
-      create: true,
-      truncate: true,
-    });
-    await writeAll(file, bundle);
-    file.close();
-  }
-  await Deno.mkdir(path, { recursive: true }).catch(() => {});
-  for (const [k, data] of Object.entries(bundle)) {
-    await write(data, `${path}/${k}`);
-  }
 };
 
 if (import.meta.main) {
